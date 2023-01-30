c811f4668ffc04803f9ee6c992970f9aad330e4e
pdaDev <dapasynkov2001@gmail.com>
2023-01-30 23:32:50 +0700

    save

--- test.js
+++
@@ -1,9 +0,0 @@
-const Wtc = require('./index')
-
-
-const wtc = new Wtc('file-structure-builder', { }).addMethod('create', mb => mb
-    .addType('folder', ['name'], tb => tb.addAction(b => b.makeStructure('',{ folder: null }))))
-
-
-wtc.launch()
-
